058304b996c5073a6f007c4a20a93de66998ccfa
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-01 21:23:53 +0530

    learning how to use public and static folders

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express  = require("express");
 const request = require("request");
 const bodyparser = require("body-parser");
 const app = express();
+// for server to serve static files such as css and images we use a special function 
+app.use(express.static("public")); //(foldername)
 
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
 
 
 app.listen(3000,function(){
